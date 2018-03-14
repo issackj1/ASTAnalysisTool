967ec59aae5fa59109e76bed82b665cc049edd52
Alan Fung <FungAlan@users.noreply.github.com>
2018-03-14 07:06:33 -0600

    tests almost done, replace paths next update

--- ASTAnalyze/src/ASTAnalyzeTest.java
+++ ASTAnalyze/src/ASTAnalyzeTest.java
@@ -1,3 +1,4 @@
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.*;
 
 /**
@@ -11,6 +12,7 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 import org.junit.Test;
 public class ASTAnalyzeTest {
@@ -31,7 +33,6 @@ public class ASTAnalyzeTest {
 		String sourcepath = BASEDIR+"TestingFiles";			// Source path from terminal argument
 		File directory = new File(sourcepath);				// Create File Object with source name
 		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
-		String codeBase = analyzer.getCodeBase(fileList);
 		
 		assertEquals(4, fileList.length);
 		
@@ -49,9 +50,11 @@ public class ASTAnalyzeTest {
 		String sourcepath = BASEDIR+"TestingFiles";			// Source path from terminal argument
 		File directory = new File(sourcepath);				// Create File Object with source name
 		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
-		String codeBase = analyzer.getCodeBase(fileList);
-		
-		assertEquals(codeBase,"b");
+		String javaFile="";
+		for (File files: fileList) {
+			javaFile = analyzer.getFile(files);
+		}
+		assertEquals(javaFile,"b");
 	}
 	
 	/**
@@ -67,9 +70,184 @@ public class ASTAnalyzeTest {
 		String sourcepath = BASEDIR+"TestingFiles";			// Source path from terminal argument
 		File directory = new File(sourcepath);				// Create File Object with source name
 		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
-		String codeBase = analyzer.getCodeBase(fileList);
-		
-		assertEquals(4, fileList.length);
-		
+		String javaFile="";
+		for (File files: fileList) {
+			javaFile = analyzer.getFile(files);
+		}
+		assertEquals(javaFile,"");
 	}
+	
+	/**
+	 * Check that a directory with empty files returns an empty string
+	 * @throws IOException 
+	 * @see getFile
+	 * @see getCodeBase
+	 */
+	@Test
+	public void testEmptyFiles() throws IOException {
+		// Prepare source code from pathfile
+		ASTAnalyze analyzer = new ASTAnalyze();
+		String sourcepath = BASEDIR+"TestingFiles";			// Source path from terminal argument
+		File directory = new File(sourcepath);				// Create File Object with source name
+		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
+		String javaFile="";
+		for (File files: fileList) {
+			javaFile = analyzer.getFile(files);
+		}
+		assertEquals(javaFile,"b");
+	}
+	
+	
+	/**
+	 * Check for a null-pointer exception when an invalid directory is provided
+	 * @throws IOException 
+	 * @see getFile
+	 * @see getCodeBase
+	 */
+	@Test(expected = NullPointerException.class)
+	public void testInvalidDirectory() throws IOException {
+		// Prepare source code from pathfile
+		ASTAnalyze analyzer = new ASTAnalyze();
+		String sourcepath = BASEDIR+"TestingFiles";			// Source path from terminal argument
+		File directory = new File(sourcepath);				// Create File Object with source name
+		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
+		String javaFile="";
+		for (File files: fileList) {
+			javaFile = analyzer.getFile(files);
+		}
+	}
+	
+	
+	/**
+	 * Check for a null-pointer exception when an invalid set of files is provided
+	 * @throws IOException 
+	 * @see getFile
+	 * @see getCodeBase
+	 */
+	@Test(expected = NullPointerException.class)
+	public void testReference() throws IOException {
+		ASTAnalyze analyzer = new ASTAnalyze();
+		String sourcepath = BASEDIR+"TestingFiles"+File.separator+;			// Source path from terminal argument
+		File directory = new File(sourcepath);				// Create File Object with source name
+		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
+		String javaFile="";
+		String targetName = "java.util."
+		ASTParser parser = analyzer.initParser(javaFile, null);
+			analyzer.parse(parser, "java.util.Class");
+		}
+	}
+	
+	
+	/**
+	 * Test if TypeDeclaration nodes are counted properly
+	 * @throws IOException 
+	 * 
+	 */	
+	@Test
+	public void testClassDeclaration() throws IOException {
+		int decCount;
+		ASTAnalyze analyzer = new ASTAnalyze();
+		String sourcepath = BASEDIR+"TestingFiles"+File.separator+;			// Source path from terminal argument
+		File directory = new File(sourcepath);				// Create File Object with source name
+		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
+		String javaFile="";
+		String targetName = "java.util."
+		for (File files: fileList) {
+			javaFile = analyzer.getFile(files);
+			ASTParser parser = analyzer.initParser(javaFile, files);
+			analyzer.parse(parser, "java.util.Class");
+		}
+		assertEquals(decCount,1);
+	}
+	
+	/**
+	 * Test if AnnotationTypeDeclaration nodes are counted properly
+	 * @throws IOException
+	 */	
+	@Test
+	public void testAnnotationDeclaration() throws IOException {
+		int decCount;
+		ASTAnalyze analyzer = new ASTAnalyze();
+		String sourcepath = BASEDIR+"TestingFiles"+File.separator+;			// Source path from terminal argument
+		File directory = new File(sourcepath);				// Create File Object with source name
+		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
+		String javaFile="";
+		String targetName = "java.util."
+		for (File files: fileList) {
+			javaFile = analyzer.getFile(files);
+			ASTParser parser = analyzer.initParser(javaFile, files);
+			analyzer.parse(parser, "java.util.Class");
+		}
+		assertEquals(decCount,1);
+	}
+	
+	/**
+	 * Test if EnumDeclaration nodes are counted properly
+	 * @throws IOException
+	 */	
+	@Test
+	public void testEnumDeclaration() throws IOException {
+		int decCount;
+		ASTAnalyze analyzer = new ASTAnalyze();
+		String sourcepath = BASEDIR+"TestingFiles"+File.separator+;			// Source path from terminal argument
+		File directory = new File(sourcepath);				// Create File Object with source name
+		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
+		String javaFile="";
+		String targetName = "java.util."
+		for (File files: fileList) {
+			javaFile = analyzer.getFile(files);
+			ASTParser parser = analyzer.initParser(javaFile, files);
+			analyzer.parse(parser, "java.util.Class");
+		}
+		assertEquals(decCount,1);
+	}
+	
+	/**
+	 * Test if references are counted properly
+	 * @throws IOException 
+	 *
+	 * 
+	 */	
+	@Test
+	public void testReference() throws IOException {
+		int refCount;
+		ASTAnalyze analyzer = new ASTAnalyze();
+		String sourcepath = BASEDIR+"TestingFiles"+File.separator+;			// Source path from terminal argument
+		File directory = new File(sourcepath);				// Create File Object with source name
+		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
+		String javaFile="";
+		String targetName = "java.util."
+		for (File files: fileList) {
+			javaFile = analyzer.getFile(files);
+			ASTParser parser = analyzer.initParser(javaFile, files);
+			analyzer.parse(parser, "java.util.Class");
+		}
+		assertEquals(refCount,1);
+	}
+	
+	/**
+	 * Test for zero references and zero declarations in an empty file
+	 * @throws IOException 
+	 * 
+	 * 
+	 */	
+	@Test
+	public void testReference() throws IOException {
+		int refCount;
+		int decCount;
+		ASTAnalyze analyzer = new ASTAnalyze();
+		String sourcepath = BASEDIR+"TestingFiles"+File.separator+;			// Source path from terminal argument
+		File directory = new File(sourcepath);				// Create File Object with source name
+		File[] fileList = directory.listFiles();			// Gets all files in directory into a File[]
+		String javaFile="";
+		String targetName = "java.util."
+		for (File files: fileList) {
+			javaFile = analyzer.getFile(files);
+			ASTParser parser = analyzer.initParser(javaFile, files);
+			analyzer.parse(parser, "java.util.Class");
+		}
+		assertEquals(refCount,0);
+		assertEquals(decCount, 0);
+	}
+	
 }
